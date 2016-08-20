8adc41bb13868db7707d1c3239205f5c0862a6b4
projetounisus <projetounisus@gmail.com>
2016-08-20 13:42:20 -0300

    Adicionando TODO

--- src/main/java/uniSusWeb/controllers/LoginController.java
+++ src/main/java/uniSusWeb/controllers/LoginController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+//TODO: implementar spring security
+
 @Controller
 @RequestMapping("/")
 public class LoginController {
