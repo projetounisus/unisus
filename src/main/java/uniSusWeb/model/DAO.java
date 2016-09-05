961b632330b3a77c64b0557594524845ec5706d5
projetounisus <projetounisus@gmail.com>
2016-09-04 21:04:06 -0300

    Remvendo código desnecessário

--- src/main/java/uniSusWeb/model/DAO.java
+++ src/main/java/uniSusWeb/model/DAO.java
@@ -10,18 +10,13 @@ import org.hibernate.cfg.Configuration;
 
 public abstract class DAO implements Modelo {	
 	protected Session getCurrentDBSession(){
-		//FIXME: isso me parece muito errado pois parece inflxível, porcurar forma melhor
-		//File file = new File("C:/ProjetoFinal/uniSusWeb/src/main/java/hibernate.cfg.xml");
-		
 		Configuration configuration = new Configuration();
-		//configuration.configure(file);
 		configuration.configure();
 		
 		StandardServiceRegistryBuilder standardServiceBuilder = configuration.getStandardServiceRegistryBuilder();
 		StandardServiceRegistry standardService = standardServiceBuilder.build();
 		
 		SessionFactory buildSessionFactory = configuration.buildSessionFactory(standardService);
-		
 		
 		return buildSessionFactory.openSession();
 	}
